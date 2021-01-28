fd28c4db45bf8dc72d813d3d5449056dd94ee7b5
randomLockNess <c.barbisan@free.fr>
2021-01-28 12:20:16 +0100

    addshield

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MLAPI;
+using UnityEngine;
+
+public class Shield : NetworkedBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IsOwner)
+        {
+            transform.parent = GameObject.FindWithTag("RightController").transform;
+            transform.position += transform.forward * transform.parent.localScale.x;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
